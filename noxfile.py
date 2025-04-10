c1371d1535146492fa4578dd2e28e3aee01fb47d
Dan Tebben <dantebben@gmail.com>
2025-04-09 20:34:47 -0400

    fail on purpose

--- noxfile.py
+++ noxfile.py
@@ -65,7 +65,7 @@ def fmt(s: Session, command: list[str]) -> None:
         ["ruff", "format", "--check", "."],
     ],
 )
-def lint(s: Session, command: list[str]) -> None:
+def lint(s: Session, command: list[str]) -> list:
     s.run(*command)
 
 

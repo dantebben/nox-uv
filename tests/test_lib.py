3af94d1846e1e4489987446130c094f5004b4cc0
Dan Tebben <dantebben@gmail.com>
2025-05-03 18:58:33 -0400

    resolved all issues from the PR

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -11,6 +11,6 @@ def test_run_uv_nox() -> None:
     cur_folder = Path.cwd()
     testing_folder = Path(__file__).parent / "subproject"
     os.chdir(testing_folder)
-    a = subprocess.run(["python3", "-m", "nox"])
+    a = subprocess.run(["uv", "run", "python", "-m", "nox"])
     assert a.returncode == 0
     os.chdir(cur_folder)

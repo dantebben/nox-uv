2d1082df99a06872304185c3511a8a67567b3f7b
dantebben <dantebben@gmail.com>
2025-06-05 10:13:37 -0400

    Update tests/test_lib.py
    Co-authored-by: johnthagen <johnthagen@users.noreply.github.com>

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -23,7 +23,7 @@ def test_run_failed_uv_venv() -> None:
     a = subprocess.run(
         ["uv", "run", "python", "-m", "nox", "-s", "failed_virtualenv"], capture_output=True
     )
-    assert a.returncode == 1  # This test is expected to fail with a `NoxUVError` raised.
+    assert a.returncode == 1  # This test is expected to fail with a `Session.error` raised.
     assert "is not allowed" in a.stderr.decode()
 
     a = subprocess.run(

238c19548e6ed8ac02ed7ddebba2cdbac9b2bcf0
John Beisley <johnbeisley@proton.me>
2026-04-01 17:43:24 +0100

    refactor subworkspace tests for composability

--- tests/subworkspace/noxfile.py
+++ tests/subworkspace/noxfile.py
@@ -6,52 +6,38 @@ options.default_venv_backend = "uv"
 
 PKG_ONE = "package-one"
 PKG_TWO = "package-two"
-PKG_ONE_BIN = "package-one-bin"
-PKG_TWO_BIN = "package-two-bin"
+
+
+def _run_binaries(s: Session) -> None:
+    for run_binary in s.posargs:
+        s.run(run_binary)
+
+
+@session
+def run_with_no_packages(s: Session) -> None:
+    _run_binaries(s)
 
 
 @session(uv_packages=[PKG_ONE])
-def run_package_one_with_its_package(s: Session) -> None:
-    s.run(PKG_ONE_BIN)
+def run_with_package_one(s: Session) -> None:
+    _run_binaries(s)
 
 
 @session(uv_packages=[PKG_TWO])
-def run_package_two_with_its_package(s: Session) -> None:
-    s.run(PKG_TWO_BIN)
+def run_with_package_two(s: Session) -> None:
+    _run_binaries(s)
 
 
 @session(uv_packages=[PKG_ONE, PKG_TWO])
-def run_both_packages_with_both_explicitly_listed(s: Session) -> None:
-    s.run(PKG_ONE_BIN)
-    s.run(PKG_TWO_BIN)
+def run_with_packages_one_and_two(s: Session) -> None:
+    _run_binaries(s)
 
 
 @session(uv_all_packages=True)
-def run_both_packages_with_all_packages(s: Session) -> None:
-    s.run(PKG_ONE_BIN)
-    s.run(PKG_TWO_BIN)
-
-
-@session
-def run_package_one_with_no_packages(s: Session) -> None:
-    s.run(PKG_ONE_BIN)
-
-
-@session
-def run_package_two_with_no_packages(s: Session) -> None:
-    s.run(PKG_TWO_BIN)
-
-
-@session(uv_packages=[PKG_TWO])
-def run_package_one_with_package_two(s: Session) -> None:
-    s.run(PKG_ONE_BIN)
-
-
-@session(uv_packages=[PKG_ONE])
-def run_package_two_with_package_one(s: Session) -> None:
-    s.run(PKG_TWO_BIN)
+def run_with_all_packages(s: Session) -> None:
+    _run_binaries(s)
 
 
 @session(uv_packages=[PKG_ONE], uv_all_packages=True)
 def run_explicit_and_all_packages(s: Session) -> None:
-    s.run(PKG_TWO_BIN)
+    _run_binaries(s)
--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -3,6 +3,11 @@ import subprocess
 
 import pytest
 
+PKG_ONE_BIN = "package-one-bin"
+PKG_TWO_BIN = "package-two-bin"
+PKG_ONE_GREETING = "Hello from package-one!"
+PKG_TWO_GREETING = "Hello from package-two!"
+
 
 @pytest.fixture(autouse=True)
 def chdir_workspace(monkeypatch: pytest.MonkeyPatch) -> None:
@@ -11,35 +16,53 @@ def chdir_workspace(monkeypatch: pytest.MonkeyPatch) -> None:
 
 
 @pytest.mark.parametrize(
-    "session_name",
+    ("session_name", "run_binary", "expect_in_stdout"),
     [
-        "run_package_one_with_its_package",
-        "run_package_two_with_its_package",
-        "run_both_packages_with_both_explicitly_listed",
-        "run_both_packages_with_all_packages",
+        ("run_with_package_one", PKG_ONE_BIN, PKG_ONE_GREETING),
+        ("run_with_package_two", PKG_TWO_BIN, PKG_TWO_GREETING),
+        ("run_with_packages_one_and_two", PKG_ONE_BIN, PKG_ONE_GREETING),
+        ("run_with_packages_one_and_two", PKG_TWO_BIN, PKG_TWO_GREETING),
+        ("run_with_all_packages", PKG_ONE_BIN, PKG_ONE_GREETING),
+        ("run_with_all_packages", PKG_TWO_BIN, PKG_TWO_GREETING),
     ],
 )
-def test_expected_success(session_name: str) -> None:
-    a = subprocess.run(["uv", "run", "python", "-m", "nox", "-s", session_name])
-    assert a.returncode == 0
+def test_expected_success(
+    session_name: str,
+    run_binary: str,
+    expect_in_stdout: str,
+) -> None:
+    a = subprocess.run(
+        ["uv", "run", "python", "-m", "nox", "-s", session_name, "--", run_binary],
+        capture_output=True,
+        text=True,
+    )
+    assert a.returncode == 0, a.stderr
+    assert expect_in_stdout in a.stdout
 
 
 @pytest.mark.parametrize(
-    ("session_name", "expect_in_error"),
+    ("session_name", "run_binary", "expect_in_stderr"),
     [
-        ("run_package_one_with_no_packages", "package-one-bin not found."),
-        ("run_package_two_with_no_packages", "package-two-bin not found."),
-        ("run_package_one_with_package_two", "package-one-bin not found."),
-        ("run_package_two_with_package_one", "package-two-bin not found."),
+        ("run_with_no_packages", PKG_ONE_BIN, "package-one-bin not found."),
+        ("run_with_no_packages", PKG_TWO_BIN, "package-two-bin not found."),
+        ("run_with_package_one", PKG_TWO_BIN, "package-two-bin not found."),
+        ("run_with_package_two", PKG_ONE_BIN, "package-one-bin not found."),
         (
             "run_explicit_and_all_packages",
+            PKG_ONE_BIN,
             "uv_packages and uv_all_packages are mututally exclusive",
         ),
     ],
 )
-def test_expected_failure(session_name: str, expect_in_error: str) -> None:
+def test_expected_failure(
+    session_name: str,
+    run_binary: str,
+    expect_in_stderr: str,
+) -> None:
     a = subprocess.run(
-        ["uv", "run", "python", "-m", "nox", "-s", session_name], capture_output=True, text=True
+        ["uv", "run", "python", "-m", "nox", "-s", session_name, "--", run_binary],
+        capture_output=True,
+        text=True,
     )
     assert a.returncode == 1
-    assert expect_in_error in a.stderr
+    assert expect_in_stderr in a.stderr
